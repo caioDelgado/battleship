61b3912033167bdcf2ceb2cf60711c1fb2d892ea
caiodelgado <kaioconrrado@hotmail.com>
2017-11-02 00:10:28 -0200

    feat: add initialize ships

--- src/services/Game.js
+++ src/services/Game.js
@@ -22,23 +22,17 @@ module.exports = class GameService {
 
   initializeShips (params) {
   	return this.gameModel.findById(params._id, (err, game) => {
-		if (err) return err
-		else {
-			const users = [
-	  			{
-	  				id: params.userId1,
-		  			life: params.life1,
-		  			ships: params.ships1
-		  		},
-		  		{
-		  			id: params.userId2,
-		  			life: params.life2,
-		  			ships: params.ships2
-		  		}
-	  		]
-			game.users = users
-			return game.save()	
-		}
-	})
+  		if (err) return err
+  		else {
+        game.users = game.users.map(user => {
+          if (user.id == params.user) {
+            user.ships = params.ship
+            user.life = params.life
+          }
+          return user
+        })
+        game.save()
+  		}
+  	})
   }
 }
